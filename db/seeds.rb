641dbba899654216e9f0e2d71d500076dc83763e
mkay0961 <mkay0961@gmail.com>
2019-03-21 13:17:56 -0400

    stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,51 @@ Game.destroy_all
 Player.destroy_all
 Card.destroy_all
 
-
 game1 = Game.create(game_status: "not started" , winner: "None")
+
 p1 = Player.create(name: "Phil")
 p2 = Player.create(name: "Matt")
+
 GamePlayer.create(game_id: game1.id, player_id: p1.id)
 GamePlayer.create(game_id: game1.id, player_id: p2.id)
-for x in (0..10)
+
+
+for x in (0..9)
   for i in ["red","yellow","green", "blue"]
     Card.create(number: x, color: i)
   end
 end
-Card.all[5].update(player_id: p1.id)
-Card.all[15].update(player_id: p1.id)
-Card.all[20].update(player_id: p1.id)
-Card.all[25].update(player_id: p1.id)
 
-deck1 = Deck.create(game_id: game1.id, card_id: Card.all[17].id )
-deck2 = Deck.create(game_id: game1.id, card_id: Card.all[18].id )
+allcards = Card.all
+allcards= allcards.shuffle
 
-Game.first.update(active_card_id: Card.all[24].id)
+  for g in game1.players
+    for i in (0..4)
+      allcards[i].update(player_id: g.id)
+      puts allcards[i].number
+      allcards.shift()
+    end
+    puts "---"
+  end
+  puts "---"
+  puts allcards.count
+  puts "---"
+
+  game1.update(active_card_id: allcards.shift().id)
+
+  for k in allcards
+    Deck.create(game_id: game1.id, card_id: k.id )
+  end
+
+
+
+
+
+
+
+
+#
+# Card.all[5].update(player_id: p1.id)
+# Card.all[15].update(player_id: p1.id)
+# Card.all[20].update(player_id: p1.id)
+# Card.all[25].update(player_id: p1.id)
