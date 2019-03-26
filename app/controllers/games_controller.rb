61984883dff6cc10d67d3fde2e394ae10c93ef30
github username <github email address>
2019-03-26 12:36:46 -0400

    i think all of the backend stuff is working now

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,44 +12,15 @@ class GamesController < ApplicationController
 
   def update
     @game = Game.find(params[:id])
-    puts "+++++++++++++++++++++++++"
-    puts @game.id
-    puts params["active_card"]
-    puts "+++++++++++++++++++++++++"
-    @active = Card.find_by(number: params["active_card"]["number"], color:params["active_card"]["color"])
-    @game.update(game_status: params["game_status"], winner: params["winner"], active_card_id: @active.id)
-
-
-    Deck.where(game_id: @game.id).destroy_all
-    params["deck"].each_with_index{|deck|
-      g = Card.find_by(number:deck["number"],color:deck["color"])
-      Deck.create(game_id: @game.id, card_id: g.id )
-    }
-
-    @game.players.each_with_index{|player, inx|
-
-      PlayerCard.where( player_id: player.id).each{|card|
-        # puts "+++++++++++++++++++++++++"puts
-        puts card.destroy
-        # puts player.id
-        # x = PlayerCard.find_by(player_id: player.id,card_id: g.id)
-        # puts "+++++++++++++++++++++++++"
-      }
-
-    }
-
-    params["players"].each_with_index{|player, inx|
-      p = Player.find_by(name: player["name"])
-      player["cards"].each_with_index{|card,x|
-        g = Card.find_by(number:card["number"],color:card["color"])
-        l= PlayerCard.create(player_id: p.id,card_id: g.id)
-       }
+    @game.update(game_status: params["game_status"], winner:params["winner"])
+    @game.cards.each_with_index{|card,x|
+      newPos = params["cards"][x]["game_position"]
+      card.update(game_position: newPos )
     }
     render json: @game
   end
 
   def create
-    puts "KJGJHGJGJKVGV"
     game = Game.newGame(params["name"])
     render json: game
 
--- app/models/game.rb
+++ app/models/game.rb
@@ -4,39 +4,45 @@ class Game < ApplicationRecord
   has_many :cards
 
   def self.newGame(name)
+
     game1 = Game.create(game_status: "not started" , winner: "None")
 
-    p1 = Player.create(name: name)
-    p2 = Player.create(name: "Comp1")
-    p3 = Player.create(name: "Comp2")
-    p4 = Player.create(name: "Comp3")
+    p1 = Player.find_or_create_by(name: name)
+    p2 = Player.find_or_create_by(name: "Comp1")
+    p3 = Player.find_or_create_by(name: "Comp2")
+    p4 = Player.find_or_create_by(name: "Comp3")
 
-    GamePlayer.create(game_id: game1.id, player_id: p1.id)
-    GamePlayer.create(game_id: game1.id, player_id: p2.id)
-    GamePlayer.create(game_id: game1.id, player_id: p3.id)
-    GamePlayer.create(game_id: game1.id, player_id: p4.id)
+    GamePlayer.create(game_id: game1.id, player_id: p1.id, seat: 0)
+    GamePlayer.create(game_id: game1.id, player_id: p2.id, seat: 1)
+    GamePlayer.create(game_id: game1.id, player_id: p3.id, seat: 2)
+    GamePlayer.create(game_id: game1.id, player_id: p4.id, seat: 3)
 
+    for x in (0..9)
+      for i in ["red","yellow","green", "blue"]
+        Card.create(number: x, color: i, game_id: game1.id)
+      end
+    end
 
-    allcards = Card.all
-    allcards= allcards.shuffle
-    allcards= allcards.shuffle
+    for x in (1..9)
+      for i in ["red","yellow","green", "blue"]
+        Card.create(number: x, color: i, game_id: game1.id)
+      end
+    end
 
-      for g in game1.players
-        for i in (0..4)
-          card = allcards.shift()
-          PlayerCard.create(player_id: g.id,card_id: card.id)
+    2.times do
+      for x in ["draw2", "reverse", "skip"]
+        for i in ["red","yellow","green", "blue"]
+          Card.create(number: x, color: i, game_id: game1.id)
         end
-        puts "---"
       end
-      puts "---"
-      puts allcards.count
-      puts "---"
 
-      game1.update(active_card_id: allcards.shift().id)
-
-      for k in allcards
-        Deck.create(game_id: game1.id, card_id: k.id )
+      for x in["color", "draw4"]
+        for i in ["wild", "wild"]
+             Card.create(number: x, color: i, game_id: game1.id)
+        end
       end
+    end
+
 
     return game1
 
